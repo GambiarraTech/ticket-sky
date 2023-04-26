94f1cdffa6215cecb9fb772ba32ac59d6c71d657
DaviJat <ddavijatoba33@gmail.com>
2023-04-26 15:52:03 -0300

    Adding index = .env

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,23 @@
+import { useState } from 'react';
+import * as router from '../pages/api/router';
+// plano: com base no método a gente vai diferenciar as funções dentro da api (if req.method =='post') ai por exemplo.
+// se for post com base no que é passado dentro do body, ele vai montar o sql la dentro, exemplo, se passar um id, adicionar uma
+// comwhere id = "valor passado". so que no api a gente só usa para identificar o methodo e fzr o destructuring no valores.
+
 export default function Home() {
+  const [cliente, setCliente] = useState({
+    email: '',
+    senha: '',
+  });
+
   return (
-    <>
-      <div>Olá, Mudo!</div>
-    </>
+    <div>
+      <input type="email" placeholder="Email" onChange={(e) => (cliente.email = e.target.value)} />
+      <input type="password" placeholder="Senha" onChange={(e) => (cliente.senha = e.target.value)} />
+
+      <button onClick={() => router.apiPost(cliente, 'cliente')}>Cadastrar-se</button>
+
+      <button onClick={() => router.apiGet('cliente')}>deletar</button>
+    </div>
   );
 }
