1efab83368981547b91e2dc98a3a845d5f135df4
DaviJat <ddavijatoba33@gmail.com>
2023-05-06 13:53:05 -0300

    Revert "Merge branch 'main' into Julia"
    This reverts commit 7e0d18860adba32d2d84a5438157ce79f70125c7, reversing
    changes made to 10ec2c9a8eebfa8f75384777057e8b6678354647.

--- src/components/admin/Footer.tsx
+++
@@ -1,9 +0,0 @@
-import styles from '@/styles/admin/footer.module.css';
-
-export default function Footer() {
-  return (
-    <footer className={styles.footer}>
-      <p>&copy; 2023 Copyright: Gambiarra Tech</p>
-    </footer>
-  );
-}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,26 +1,16 @@
 /** @type {import('tailwindcss').Config} */
-const colors = require('tailwindcss/colors');
 module.exports = {
-  content: ['./src/*/.{js,jsx,ts,tsx}'],
+  content: [
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
   theme: {
-    extend: {},
-    colors: {
-      'azul-ticket': '#0112a6',
-      'ciano-ticket': '#03f7ff',
-      'ticket-sky-blue': '#000D67',
-      'green-500': '#22c55e',
-      'green-600': '#16a34a',
-      'green-700': '#15803d',
-      'gray-700': '#374151',
-      'blue-100': '#dbeafe',
-      'blue-200': '#bfdbfe',
-      'blue-700': '#1d4ed8',
-      'slate-500': '#64748b',
-      white: colors.white,
-    },
-    backgroundImage: {
-      'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-      'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+    extend: {
+      backgroundImage: {
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      },
     },
   },
   plugins: [],
