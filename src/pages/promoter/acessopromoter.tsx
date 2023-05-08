e3f4aa0dd87c53f352541886cdddb6ed6ec12e8a
DaviJat <ddavijatoba33@gmail.com>
2023-05-08 14:08:55 -0300

    Corrigindo rota de style

--- src/pages/promoter/acessopromoter.tsx
+++ src/pages/promoter/acessopromoter.tsx
@@ -1,4 +1,4 @@
-import style from '@/styles/promoter/acessopromoter.module.css';
+import style from '@/styles/promoter/acessoPromoter.module.css';
 import { Inter } from 'next/font/google';
 import Head from 'next/head';
 import Image from 'next/image';
