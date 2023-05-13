d816e19341fab698c27c1d41a7bda1f7d1cddbd5
DaviJat <91758001+DaviJat@users.noreply.github.com>
2023-05-13 11:22:24 -0300

    Mudando index cliente para o index principal

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,25 @@
+import NavbarCliente from '@/components/cliente/navbar/Navbar';
+import Carousel from '@/components/promoter/Carousel';
+import { AuthContext } from '@/contexts/AuthContext';
+import { Inter } from 'next/font/google';
+import Head from 'next/head';
+import { useContext } from 'react';
+
+const font = Inter({ subsets: ['latin'], weight: '500' });
+
 export default function Home() {
-  return <div>HomePage</div>;
+  const { user, isLogged, logout, autenticar } = useContext(AuthContext);
+
+  autenticar('/');
+  return (
+    <main className={`${font.className}`}>
+      <Head>
+        <title>TicketSky - Página Inicial</title>
+      </Head>
+      <NavbarCliente />
+      <Carousel title="Em Alta" page="cliente/telaevento"></Carousel>
+      <Carousel title="Vistos Recentemente"></Carousel>
+      <Carousel title="Pra você"></Carousel>
+    </main>
+  );
 }
