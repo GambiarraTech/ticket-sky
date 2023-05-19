1370dfd59fe7e78cedbc2b63be577aa9e819ebaa
DaviJat <91758001+DaviJat@users.noreply.github.com>
2023-05-19 10:18:16 -0300

    Estrutura inicial da nova NavBar

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,17 @@
+import style from '@/styles/navbar.module.css';
+
+export default function Navbar() {
+  return (
+    <nav className={style.navbar}>
+      <div className={style.container}>
+        <p>teste123412341234123411234</p>
+      </div>
+      <div className={style.container}>
+        <p>teste123412341234123411234</p>
+      </div>
+      <div className={style.container}>
+        <p>teste1234444444444444444444444444444</p>
+      </div>
+    </nav>
+  );
+}
