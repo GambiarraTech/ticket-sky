e0db23b1d5053233d06f27d5ef7635e97a65a381
DaviJat <91758001+DaviJat@users.noreply.github.com>
2023-05-19 11:12:52 -0300

    Modelo de componentização

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,17 +1,20 @@
 import style from '@/styles/navbar.module.css';
 
-export default function Navbar() {
+type NavBarProps = {
+  backgroundColor: string;
+  leftComponent?: React.ReactNode;
+  centerComponent?: React.ReactNode;
+  rightComponent?: React.ReactNode;
+};
+
+export default function Navbar(props: NavBarProps) {
   return (
-    <nav className={style.navbar}>
-      <div className={style.container}>
-        <p>teste123412341234123411234</p>
-      </div>
-      <div className={style.container}>
-        <p>teste123412341234123411234</p>
-      </div>
-      <div className={style.container}>
-        <p>teste1234444444444444444444444444444</p>
-      </div>
-    </nav>
+    <div style={{ backgroundColor: props.backgroundColor }}>
+      <nav className={style.navbar}>
+        <div className={style.container}>{props.leftComponent}</div>
+        <div className={style.container}>{props.centerComponent}</div>
+        <div className={style.container}>{props.rightComponent}</div>
+      </nav>
+    </div>
   );
 }
--- src/pages/playground.tsx
+++ src/pages/playground.tsx
@@ -1,5 +1,13 @@
 import Navbar from '@/components/Navbar';
+import Image from 'next/image';
 
-export default function Home() {
-  return <Navbar />;
+export default function Playground() {
+  return (
+    <Navbar
+      backgroundColor="#000D67"
+      leftComponent={<Image src="/images/logo-navbar-white.png" alt="TicketSky - Logo" height="120" width="120" />}
+      centerComponent={<div>Teste</div>}
+      rightComponent={<button>Botao</button>}
+    />
+  );
 }
