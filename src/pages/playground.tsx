a8eb1d19eaf288cb37e9e27527d49b5704390e02
DaviJat <91758001+DaviJat@users.noreply.github.com>
2023-05-20 11:22:11 -0300

    Update playground.tsx

--- src/pages/playground.tsx
+++ src/pages/playground.tsx
@@ -6,8 +6,8 @@ export default function Playground() {
     <Navbar
       backgroundColor="#000D67"
       leftComponent={<Image src="/images/logo-navbar-white.png" alt="TicketSky - Logo" height="120" width="120" />}
-      centerComponent={<div>Teste</div>}
-      rightComponent={<button>Botao</button>}
+      centerComponent={<div style={{ color: '#fff' }}>Teste</div>}
+      rightComponent={<button style={{ color: '#fff' }}>Botao</button>}
     />
   );
 }
