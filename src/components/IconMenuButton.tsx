c3087f42cc0da870032d6e99f2d1125c4f18507d
DaviJat <ddavijatoba33@gmail.com>
2023-05-23 00:10:15 -0300

    Passando botões por props

--- src/components/IconMenuButton.tsx
+++ src/components/IconMenuButton.tsx
@@ -1,8 +1,13 @@
 import style from '@/styles/IconMenuButton.module.css';
-import { useState } from 'react';
+import { MenuButtons } from '@/types/components/menuButtons';
+import React, { useState } from 'react';
 import { FaUserCircle } from 'react-icons/fa';
 
-export default function IconMenuButton() {
+type IconMenuButtonProps = {
+  menuButtons: MenuButtons[];
+};
+
+export default function IconMenuButton(props: IconMenuButtonProps) {
   // Estado para controlar a abertura e fechamento do menu
   const [menuOpen, setMenuOpen] = useState(false);
 
@@ -20,9 +25,12 @@ export default function IconMenuButton() {
         {menuOpen && (
           <div className={style.menu}>
             <ul>
-              <li>teste</li>
-              <li>teste</li>
-              <li>teste</li>
+              {props.menuButtons.map((button) => (
+                // A key serve para garantir que cada fragmento tenha uma chave exclusiva com base no input.id
+                <React.Fragment key={button.id}>
+                  <li>{button.text}</li>
+                </React.Fragment>
+              ))}
             </ul>
           </div>
         )}
--- src/pages/playground.tsx
+++ src/pages/playground.tsx
@@ -2,13 +2,18 @@ import IconMenuButton from '@/components/IconMenuButton';
 import Navbar from '@/components/Navbar';
 import Image from 'next/image';
 
+const listaButtons = [
+  { id: 'modal1', text: 'modal1' },
+  { id: 'modal2', text: 'modal2' },
+];
+
 export default function Playground() {
   return (
     <Navbar
       backgroundColor="#000D67"
       leftComponent={<Image src="/images/logo-navbar-white.png" alt="TicketSky - Logo" height="120" width="120" />}
       centerComponent={<div style={{ color: '#fff' }}>Teste</div>}
-      rightComponent={<IconMenuButton />}
+      rightComponent={<IconMenuButton menuButtons={listaButtons} />}
     />
   );
 }
