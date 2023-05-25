a9f06b3cc777be4be613562026af1ba5df9320c3
luanbsc <luanbscosta@hotmail.com>
2023-05-25 00:09:00 -0300

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,9 @@
+<<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< Updated upstream
+=======
+>>>>>>> parent of 600dd34 (Add files via upload)
 /** @type {import('tailwindcss').Config} */
 const colors = require('tailwindcss/colors');
 module.exports = {
@@ -22,18 +28,105 @@ module.exports = {
       'gray-400': '#9ca3af',
       'gray-500': '#6b7280',
       'gray-700': '#374151',
+<<<<<<< HEAD
+      'gray-800': '#1f2937',
+=======
+>>>>>>> parent of 600dd34 (Add files via upload)
       'gray-900': '#111827',
       'gray-950': '#020617',
       'gray-50': '#F9FAFB',
       'blue-100': '#dbeafe',
       'blue-200': '#bfdbfe',
       'blue-700': '#1d4ed8',
+<<<<<<< HEAD
+      'blue-900': '#1e3a8a',
       'slate-500': '#64748b',
       'red-600': '#dc2626',
       'red-700': '#b91c1c',
+      'red-800': '#991b1b',
+=======
+      'slate-500': '#64748b',
+      'red-600': '#dc2626',
+      'red-700': '#b91c1c',
+>>>>>>> parent of 600dd34 (Add files via upload)
       black: colors.black,
       white: colors.white,
     },
   },
   plugins: [],
 };
+<<<<<<< HEAD
+=======
+=======
+>>>>>>> parent of 600dd34 (Add files via upload)
+=======
+>>>>>>> parent of 600dd34 (Add files via upload)
+/** @type {import('tailwindcss').Config} */
+const colors = require('tailwindcss/colors');
+module.exports = {
+  content: ['./src/*/.{js,jsx,ts,tsx}'],
+  theme: {
+    extend: {
+      backgroundImage: {
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      },
+    },
+    colors: {
+      'azul-ticket': '#0112a6',
+      'hoover-blue': '#0028be',
+      'ciano-ticket': '#03f7ff',
+      'ticket-sky-blue': '#000D67',
+      'green-500': '#22c55e',
+      'green-600': '#16a34a',
+      'green-700': '#15803d',
+      'gray-200': '#e5e7eb',
+      'gray-300': '#D1D5DB',
+      'gray-400': '#9ca3af',
+      'gray-500': '#6b7280',
+      'gray-700': '#374151',
+<<<<<<< HEAD
+<<<<<<< HEAD
+      'gray-800': '#1f2937',
+=======
+>>>>>>> parent of 600dd34 (Add files via upload)
+=======
+>>>>>>> parent of 600dd34 (Add files via upload)
+      'gray-900': '#111827',
+      'gray-950': '#020617',
+      'gray-50': '#F9FAFB',
+      'blue-100': '#dbeafe',
+      'blue-200': '#bfdbfe',
+      'blue-700': '#1d4ed8',
+<<<<<<< HEAD
+<<<<<<< HEAD
+      'blue-900': '#1e3a8a',
+      'slate-500': '#64748b',
+      'red-600': '#dc2626',
+      'red-700': '#b91c1c',
+      'red-800': '#991b1b',
+=======
+      'slate-500': '#64748b',
+      'red-600': '#dc2626',
+      'red-700': '#b91c1c',
+>>>>>>> parent of 600dd34 (Add files via upload)
+=======
+      'slate-500': '#64748b',
+      'red-600': '#dc2626',
+      'red-700': '#b91c1c',
+>>>>>>> parent of 600dd34 (Add files via upload)
+      black: colors.black,
+      white: colors.white,
+    },
+  },
+  plugins: [],
+};
+<<<<<<< HEAD
+<<<<<<< HEAD
+>>>>>>> Stashed changes
+=======
+>>>>>>> parent of 600dd34 (Add files via upload)
+=======
+>>>>>>> parent of 600dd34 (Add files via upload)
+=======
+>>>>>>> parent of 600dd34 (Add files via upload)
