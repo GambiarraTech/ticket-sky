fbbfdb1eb3b1d253da2b5c93ead2151f198581ff
DaviJat <ddavijatoba33@gmail.com>
2023-05-30 10:48:45 -0300

    Mudando tela de cartão para modal

---
+++ src/components/cliente/ModalMeuCartao.tsx
@@ -0,0 +1,92 @@
+import * as router from '@/pages/api/router';
+import style from '@/styles/cliente/meuCartao.module.css';
+import { useState } from 'react';
+
+export default function MeuCartao() {
+  const [cartao] = useState({
+    titular: '',
+    cpf: '',
+    numero: '',
+    validade: '',
+    cvv: '',
+  });
+
+  async function cadastrarCartao() {
+    const res = await router.apiPost(cartao, 'cartao');
+
+    if (res.success) {
+    } else {
+    }
+  }
+
+  async function deletarCartao(cpf: string, numero: string) {
+    try {
+      await router.apiDelete(cartao, 'cartao');
+      return { success: true };
+    } catch (error) {
+      console.error('Erro ao remover cartão:', error);
+      return { success: false };
+    }
+  }
+
+  return (
+    <>
+      <div className={style.title}>Meu Cartão</div>
+      <form className={style.formStyle}>
+        <div className={style.inputFormat}>
+          Nome do Titular
+          <input
+            type="text"
+            className={style.primaryInputStyle}
+            placeholder="Nome do Titular"
+            onChange={(e) => (cartao.titular = e.target.value)}
+          />
+        </div>
+        <div className={style.inputFormat}>
+          CPF do Titular
+          <input
+            type="text"
+            className={style.primaryInputStyle}
+            placeholder="CPF do Titular"
+            onChange={(e) => (cartao.cpf = e.target.value)}
+          />
+        </div>
+        <div className={style.inputFormat}>
+          Número do Cartão
+          <input
+            type="text"
+            className={style.primaryInputStyle}
+            placeholder="Número do Cartão"
+            onChange={(e) => (cartao.numero = e.target.value)}
+          />
+        </div>
+        <div className={style.secondFormat}>
+          <div className={style.inputFormat}>
+            Validade
+            <input
+              type="text"
+              className={style.secondInputStyle}
+              placeholder="Validade"
+              onChange={(e) => (cartao.validade = e.target.value)}
+            />
+          </div>
+          <div className={style.inputFormat}>
+            CVV
+            <input
+              type="text"
+              className={style.secondInputStyle}
+              placeholder="CVV"
+              onChange={(e) => (cartao.cvv = e.target.value)}
+            />
+          </div>
+        </div>
+      </form>
+      <div className={style.buttonDiv}>
+        <button className={style.buttonDelete}>Remover</button>
+        <button className={style.buttonSave} onClick={cadastrarCartao}>
+          Salvar
+        </button>
+      </div>
+    </>
+  );
+}
--- src/components/cliente/NavbarCliente.tsx
+++ src/components/cliente/NavbarCliente.tsx
@@ -8,12 +8,14 @@ import { HiOutlineLogout } from 'react-icons/hi';
 import IconMenuButton from '../IconMenuButton';
 import Modal from '../Modal';
 import ModalLogin from './ModalLogin';
+import ModalMeuCartao from './ModalMeuCartao';
 import ModalMeuPerfil from './ModalMeuPerfil';
 
 export default function NavbarCliente() {
   const { user, isLogged, logout } = useContext(AuthContext);
   const [openModalLogin, setOpenModalLogin] = useState(false);
   const [openModalMeuPerfil, setOpenModalMeuPerfil] = useState(false);
+  const [openModalMeuCartao, setOpenModalMeuCartao] = useState(false);
 
   return (
     <>
@@ -29,9 +31,7 @@ export default function NavbarCliente() {
           rightComponent={
             <IconMenuButton color="#0013a6">
               <li onClick={() => setOpenModalMeuPerfil(true)}>Meu Perfil</li>
-              <li>
-                <Link href="/cliente/meuCartao">Meu Cartão</Link>
-              </li>
+              <li onClick={() => setOpenModalMeuCartao(true)}>Meu Cartão</li>
               <li>
                 <Link href="/cliente/meusIngressos">Meus Ingressos</Link>
               </li>
@@ -68,9 +68,11 @@ export default function NavbarCliente() {
       <Modal isOpen={openModalLogin} onClose={() => setOpenModalLogin(false)}>
         <ModalLogin onSubmit={() => setOpenModalLogin(false)} />
       </Modal>
-
       <Modal isOpen={openModalMeuPerfil} onClose={() => setOpenModalMeuPerfil(false)}>
         <ModalMeuPerfil />
+      </Modal>
+      <Modal isOpen={openModalMeuCartao} onClose={() => setOpenModalMeuCartao(false)}>
+        <ModalMeuCartao />
       </Modal>
     </>
   );
