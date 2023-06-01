85f3e3bbc52671dda9364e2ed8bfe4f02a0d42bd
DaviJat <ddavijatoba33@gmail.com>
2023-06-01 18:36:44 -0300

    Ajuste caroussel

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,4 @@
+import Footer from '@/components/Footer';
 import NavbarCliente from '@/components/cliente/NavbarCliente';
 import Carousel from '@/components/promoter/Carousel';
 import { AuthContext } from '@/contexts/AuthContext';
@@ -11,14 +12,19 @@ export default function Home() {
   const { user, isLogged, logout } = useContext(AuthContext);
 
   return (
-    <main className={`${font.className}`}>
-      <Head>
-        <title>TicketSky - Página Inicial</title>
-      </Head>
-      <NavbarCliente />
-      <Carousel title="Em Alta" page="cliente/telaevento"></Carousel>
-      <Carousel title="Vistos Recentemente"></Carousel>
-      <Carousel title="Pra você"></Carousel>
-    </main>
+    <>
+      <main className={`${font.className}`}>
+        <Head>
+          <title>TicketSky - Página Inicial</title>
+        </Head>
+        <NavbarCliente />
+        <div>
+          <Carousel title="Em Alta" page="cliente/telaevento"></Carousel>
+          <Carousel title="Vistos Recentemente"></Carousel>
+          <Carousel title="Pra você"></Carousel>
+        </div>
+      </main>
+      <Footer color="white" />
+    </>
   );
 }
