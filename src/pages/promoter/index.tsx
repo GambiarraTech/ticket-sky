8f7e3194e94138f561716c7315af282747a7dc86
DaviJat <91758001+DaviJat@users.noreply.github.com>
2023-06-03 08:36:15 -0300

    Ajustes

--- src/pages/promoter/index.tsx
+++ src/pages/promoter/index.tsx
@@ -6,12 +6,12 @@ import styles from '@/styles/promoter/home.module.css';
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <>
       <NavbarPromoter />
       <p className={styles.title}>Meus Eventos</p>
       <Carousel />
       <Footer color="white" />
-    </div>
+    </>
   );
 }
 
