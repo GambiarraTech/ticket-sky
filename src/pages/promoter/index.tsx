3e18d717e21881dfb9e6a42ba61b64a79925bb63
Humberto Costa <bandeiracosta67@gmail.com>
2023-06-05 15:45:08 -0300

    Tela meus eventos
    Coloquei uma mensagem caso não exista eventos cadastrados

--- src/pages/promoter/index.tsx
+++ src/pages/promoter/index.tsx
@@ -3,19 +3,36 @@ import Carousel from '@/components/promoter/Carousel';
 import NavbarPromoter from '@/components/promoter/NavbarPromoter';
 import { getServerSideProps } from '@/lib/auth';
 import styles from '@/styles/promoter/home.module.css';
+import { useState } from 'react';
+import { apiGet } from '../api/router';
+
+
 
 export default function Home() {
+    const [eventos, setEventos] = useState([]);
+    const [teste, setTeste] = useState(true);
+
+    apiGet('evento').then((value) => {
+        setEventos(value.result);
+        if(!eventos){
+
+            setTeste(false)
+        }
+    });
+
+
   return (
     <>
       <NavbarPromoter />
-      <p className={styles.title}>Meus Eventos</p>
-      <div>
-        <Carousel />
-      </div>
-
-      <Footer color="white" />
+        <p className={styles.title}>Meus Eventos</p>
+        <p className={styles.msg}>{teste ? "" : 'Nenhum Evento Cadastrado'}</p>
+        <div>
+          <Carousel />
+        </div>
+        <div className={styles.foter}>
+            <Footer color="white" />
+        </div>
     </>
   );
 }
-
 export { getServerSideProps };
