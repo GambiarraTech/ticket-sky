8778b063d2f7924ee799758de81dbcc9dd2a501b
DaviJat <ddavijatoba33@gmail.com>
2023-06-10 16:57:24 -0300

    Ajuste meu perfil

--- src/components/cliente/ModalMeuPerfil.tsx
+++ src/components/cliente/ModalMeuPerfil.tsx
@@ -20,35 +20,42 @@ export default function ModalMeuPerfil() {
   });
 
   async function editarCliente() {
-    cliente.service = 'editarCliente'
+    cliente.service = 'editarCliente';
     const res = await router.apiPost(cliente, 'cliente');
     alert(res.result);
   }
 
   useEffect(() => {
-    router.apiPost({service: 'getPerfil', id: user.id},'cliente').then((value) =>{
-        if( value.result != null){
-            setCliente(value.result);
-        }
-    }
-
-    )
+    router.apiPost({ service: 'getPerfil', id: user.id }, 'cliente').then((value) => {
+      if (value.result != null) {
+        setCliente(value.result);
+      }
+    });
   }, []);
 
   return (
     <>
       <div className={styles.container}>
         <div className={styles.title}>
-            <h1>
-                Meu Perfil
-            </h1>
+          <h1>Meu Perfil</h1>
         </div>
-        <label>Nome</label>
-        <input type="text" defaultValue={cliente.nome} onChange={(e) => (cliente.nome = e.target.value)} required/>
-        <label>Sobrenome</label>
-        <input type="text" defaultValue={cliente.sobrenome} onChange={(e) => (cliente.sobrenome = e.target.value)} required/>
         <label>Email</label>
-        <input type="email" defaultValue={cliente.email} onChange={(e) => (cliente.email = e.target.value)} required/>
+        <input
+          type="email"
+          readOnly
+          defaultValue={cliente.email}
+          onChange={(e) => (cliente.email = e.target.value)}
+          required
+        />
+        <label>Nome</label>
+        <input type="text" defaultValue={cliente.nome} onChange={(e) => (cliente.nome = e.target.value)} required />
+        <label>Sobrenome</label>
+        <input
+          type="text"
+          defaultValue={cliente.sobrenome}
+          onChange={(e) => (cliente.sobrenome = e.target.value)}
+          required
+        />
         <label>CPF</label>
         <input type="text" defaultValue={cliente.cpf} onChange={(e) => (cliente.cpf = e.target.value)} />
         <div className={styles.buttonsContainer}>
