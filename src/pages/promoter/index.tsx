beabc76451f2d97b4e4de6844dd08e03eca6e7d2
luanbsc <luanbscosta@hotmail.com>
2023-06-12 16:06:12 -0300

    Update home

--- src/pages/promoter/index.tsx
+++ src/pages/promoter/index.tsx
@@ -1,23 +1,35 @@
 import Footer from '@/components/Footer';
 import Carousel from '@/components/promoter/Carousel';
 import NavbarPromoter from '@/components/promoter/NavbarPromoter';
+import { AuthContext } from '@/contexts/AuthContext';
 import { getServerSideProps } from '@/lib/auth';
+import { apiGet } from '@/pages/api/router';
 import styles from '@/styles/promoter/home.module.css';
-import { useState } from 'react';
+import { useContext, useEffect, useState } from 'react';
 
 export default function Home() {
   const [eventos, setEventos] = useState([]);
-  const [teste, setTeste] = useState(true);
+  const [teste, setTeste] = useState(false);
+  const { user, isLogged } = useContext(AuthContext);
 
-  if (!Carousel || !Carousel.length) {
-    setTeste(false);
-  }
+  useEffect(() => {
+    if (isLogged && user.role == 'promoter') {
+      apiGet(`evento?id=${user.id}`).then((value) => {
+        setEventos(value.result);
+      });
+    }
+    if (eventos.length == 0) {
+      setTeste(true);
+    } else {
+      setTeste(false);
+    }
+  }, [user]);
 
   return (
     <>
       <NavbarPromoter />
       <p className={styles.title}>Meus Eventos</p>
-      <p className={styles.msg}>{teste ? '' : 'Nenhum Evento Cadastrado'}</p>
+      <p className={styles.msg}>{teste ? 'Nenhum Evento Cadastrado' : ''}</p>
       <div>
         <Carousel />
       </div>
